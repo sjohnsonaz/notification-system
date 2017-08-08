c39198eeed8518a17f7be691ce2ec4806cfa13d9
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-08-08 14:56:36 -0700

    Removed phonegap-plugin-push.

--- src/scripts/main.tsx
+++ src/scripts/main.tsx
@@ -14,13 +14,20 @@ window['app'] = app;
 window.onload = function () {
     if (typeof cordova !== 'undefined') {
         document.addEventListener('deviceready', function initialize() {
+            /*
+            fcmPlugin.onTokenRefresh((token) => {
+                console.log(token);
+            });
+
+            fcmPlugin.onNotification((data) => {
+                console.log(JSON.stringify(data));
+            });
             let push = PushNotification.init({
                 android: {
                     senderID: '77088182555'
                 },
                 ios: {
                     sound: true,
-                    vibration: true,
                     badge: true
                 },
                 windows: {
@@ -38,7 +45,25 @@ window.onload = function () {
             push.on('error', function (e) {
                 console.log('push error : ' + e.message);
             });
+
+            push.on('notification', function (data) {
+                console.log('notification event');
+                var cards = document.getElementById("cards");
+                var push = '<div class="row">' +
+                    '<div class="col s12 m6">' +
+                    '  <div class="card darken-1">' +
+                    '    <div class="card-content black-text">' +
+                    '      <span class="card-title black-text">' + data.title + '</span>' +
+                    '      <p>' + data.message + '</p>' +
+                    '      <p>' + data.additionalData.foreground + '</p>' +
+                    '    </div>' +
+                    '  </div>' +
+                    ' </div>' +
+                    '</div>';
+                cards.innerHTML += push;
+            });
             app['push'] = push;
+            */
             app.initialize();
         }, false);
     } else {
