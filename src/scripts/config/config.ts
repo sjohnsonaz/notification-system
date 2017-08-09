60dee809c592477f935aed03332e31622a9a8bce
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-08-09 16:17:54 -0700

    Revert "Removed phonegap-plugin-push."
    This reverts commit c39198eeed8518a17f7be691ce2ec4806cfa13d9.

--- src/scripts/config/config.ts
+++ src/scripts/config/config.ts
@@ -1,8 +1,8 @@
 import { IConfig } from '../interfaces/data/IConfig';
 
 var config: IConfig = {
-    notificationAddress: "http://10.42.1.76:3001/",
-    subscriptionAddress: "http://10.42.1.76:3000/api/subscription"
+    notificationAddress: "http://localhost:3001/",
+    subscriptionAddress: "http://localhost:3000/api/subscription"
 };
 
 export default config;
